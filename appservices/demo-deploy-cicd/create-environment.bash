b41445472ae0ed0b9c5f200003409d2d98b09e56
Paulo Baima <padasil@microsoft.com>
2020-11-08 19:36:20 -0300

    no message

--- appservices/demo-deploy-cicd/create-environment.bash
+++ appservices/demo-deploy-cicd/create-environment.bash
@@ -6,3 +6,5 @@ az group create -l $LOCATION -n $RG_NAME
 
 az appservice plan create -n $NAME -g $RG_NAME --sku S1 --location $LOCATION --is-linux
 az webapp create -g $RG_NAME -p $NAME -n $NAME -i "nginx"
+
+az acr create --resource-group $RG_NAME --name deepdiveweekcontainer --sku Basic
